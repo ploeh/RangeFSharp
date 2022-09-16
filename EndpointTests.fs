75da498ed01b09c28499bde3633854e1e87d0d66
Mark Seemann <mark@ploeh.dk>
2022-09-16 20:50:22 +0200

    Demonstrate second functor law

--- EndpointTests.fs
+++ EndpointTests.fs
@@ -12,3 +12,14 @@ let ``First functor law`` () = Property.check <| property {
     let actual = Endpoint.map id expected
 
     expected =! actual }
+
+[<Fact>]
+let ``Second functor law`` () = Property.check <| property {
+    let genInt32 = Gen.int32 (Range.linearBounded ())
+    let! endpoint = Gen.choice [Gen.map Open genInt32; Gen.map Closed genInt32]
+    let! f = Gen.item [id; ((+) 1); ((*) 2)]
+    let! g = Gen.item [id; ((+) 1); ((*) 2)]
+
+    let actual = Endpoint.map (f << g) endpoint
+
+    Endpoint.map f (Endpoint.map g endpoint) =! actual }
